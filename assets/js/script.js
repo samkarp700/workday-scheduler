4f996534efbc505d331db4ef92ca1cb7fba75cd5
Sam Karp <sfcypret@gmail.com>
2022-07-18 00:19:07 -0400

    js function outline completed

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//Main functions:
+//open planner -> current day displayed in jumbotron container 
+//time blocks for the day of the week shown - each time block is color coded to past present future
+// click into time block - can enter event
+//save button saves event to local storage 
+//refreshed page - saved event displayed on the page. 
+
+
+
+//get current day from moment.js
+
+//jumbotron - show current day 
+
+
+
+
+
