8c209bc6c9d365aac2151e8764353d8f84c38879
Sam Karp <sfcypret@gmail.com>
2022-07-18 14:55:24 -0400

    local storage working. load page placing input in all timeslots instead of just 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,21 @@
 //refreshed page - saved event displayed on the page. 
 
 //variables for time slots
-
+var nineEl = $("#9");
+var tenEl = $("#10");
+var elevenEl = $("#11");
+var twelveEl = $("#12");
+var oneEl = $("#1");
+var twoEl = $("#2");
+var threeEl = $("#3");
+var fourEl = $("#4");
+var fiveEl = $("#5");
 
 //get current day from moment.js
 var currentDate = moment().format("[Today is] dddd, MMMM Do YYYY") 
 
 //jumbotron - show current day 
 document.getElementById("currentDay").innerHTML = currentDate;
-
-//get text from event input
 
 
 //save button saves event input to local storage
@@ -32,8 +38,26 @@ $("#button .saveBtn").click(function(){
     localStorage.setItem(currentHour, JSON.stringify(eventInput));
 });
 
+//load saved events from local storage
+var loadEvents = function() {
+    var getEvent9 = JSON.parse(localStorage.getItem("9:00 A.M."));
+    console.log(getEvent9);
+    $("input").val(getEvent9);
+    var getEvent10 = JSON.parse(localStorage.getItem("10:00 A.M."));
+    var getEvent11 = JSON.parse(localStorage.getItem("11:00 A.M."));
+    var getEvent12 = JSON.parse(localStorage.getItem("12:00 P.M."));
+    var getEvent1 = JSON.parse(localStorage.getItem("1:00 P.M."));
+    var getEvent2 = JSON.parse(localStorage.getItem("2:00 P.M."));
+    var getEvent3 = JSON.parse(localStorage.getItem("3:00 P.M."));
+    var getEvent4 = JSON.parse(localStorage.getItem("4:00 P.M."));
+    var getEvent5 = JSON.parse(localStorage.getItem("5:00 P.M."));
+}
+
 
 //rows change color based on past/present/future time
 
 
 
+$(document).ready(function() {
+    loadEvents();
+})
